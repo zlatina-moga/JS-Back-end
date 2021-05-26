2975ec08adc1683898d3c5f96fa994390066c56d
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-05-26 15:10:38 +0300

    Update index.js

--- Server-demo/index.js
+++ Server-demo/index.js
@@ -1,14 +1,18 @@
 const http = require('http');
-
 const router = require('./router')
 
 const homeController = require('./controllers/homeController')
 const aboutController = require('./controllers/aboutController');
 const catalogController = require('./controllers/catalogController');
+const createController = require('./controllers/createController');
+const deleteController = require('./controllers/deleteController');
 
-router.registerHandler('/', homeController)
-router.registerHandler('/about', aboutController)
-router.registerHandler('/catalog', catalogController)
+router.get('/', homeController)
+router.get('/about', aboutController)
+router.get('/catalog', catalogController)
+
+router.post('/create', createController)
+router.get('/delete', deleteController)
 
 const port = 3000;
 const server = http.createServer(requestHandler)
@@ -16,8 +20,10 @@ const server = http.createServer(requestHandler)
 
 
 function requestHandler(req, res){
+    const url = new URL(req.url, 'http://localhost')
     console.log('>>>', req.method, req.url)
-    const handler = router.match(req.url)
+    
+    const handler = router.match(req.method, url.pathname)
     handler(req, res);
 }
 
