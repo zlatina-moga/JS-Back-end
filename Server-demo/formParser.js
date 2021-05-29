cb61d67f8d7474ee9259a36bb61b1c00a833e2ed
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-05-29 14:17:20 +0300

    Add files via upload

---
+++ Server-demo/formParser.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+async function parse(req){
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', data => body += data)
+        req.on('end', () => {
+            let result;
+            try{
+                result = body
+                    .split('&')
+                    .map(p => p.split('='))
+                    .reduce((a, c) => Object.assign(a, {[c[0]] : c[1]}), {});
+                resolve(result)
+            } catch(err) {
+                reject(err)
+            }
+        })
+    })
+}
+
+async function demo(){
+    const src = fs.createReadStream('./data.txt');
+    const result = await parse(src)
+
+    console.log(result)
+
+}
+
+demo()
