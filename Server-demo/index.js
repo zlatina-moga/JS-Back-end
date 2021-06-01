a31fd06b410adcdd0987a44e205b2617a31c3b8e
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-06-01 10:40:31 +0300

    Update index.js

--- Server-demo/index.js
+++ Server-demo/index.js
@@ -6,21 +6,23 @@ const aboutController = require('./controllers/aboutController');
 const catalogController = require('./controllers/catalogController');
 const createController = require('./controllers/createController');
 const deleteController = require('./controllers/deleteController');
+const uploadController = require('./controllers/uploadController');
 
 router.get('/', homeController)
 router.get('/about', aboutController)
 router.get('/catalog', catalogController)
+router.get('/delete', deleteController)
 
 router.post('/create', createController)
-router.get('/delete', deleteController)
+router.post('/upload', uploadController)
+
 
 const port = 3000;
 const server = http.createServer(requestHandler)
 
 
-
 function requestHandler(req, res){
-    const url = new URL(req.url, 'http://localhost')
+    const url = new URL(req.url, 'http://localhost') 
     console.log('>>>', req.method, req.url)
     
     const handler = router.match(req.method, url.pathname)
