ea030223eda16275b9471728e551c8b3c92fb525
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-06-01 10:36:46 +0300

    Update aboutController.js

--- Server-demo/controllers/aboutController.js
+++ Server-demo/controllers/aboutController.js
@@ -1,12 +1,7 @@
-const layout = require('../views/layout')
+const {layout, loadTemplate} = require('../template');
 
-const aboutPage = `
-<div>
-    <h1>About us</h1>
-    <p>About Page</p>
-</div>`;
-
-module.exports = (req, res) => {
-    res.write(layout(aboutPage, 'About'))
+module.exports = async (req, res) => {
+    const aboutPage = await loadTemplate('about')
+    res.write(await layout(aboutPage, 'About'))
     res.end()
 }
