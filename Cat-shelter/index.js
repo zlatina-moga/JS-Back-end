db779a245de6a393a08c1d7fb9bd85051720492e
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-06-09 08:54:15 +0300

    Update index.js

--- Cat-shelter/index.js
+++ Cat-shelter/index.js
@@ -4,14 +4,25 @@ const url = require('url')
 const path = require('path');
 const fs = require('fs')
 
-const viewsPath = path.resolve(config.viewsDir)
+const viewsPath = path.resolve(config.viewsDir);
+const staticFilesPath = path.resolve(config.staticFilesDir);
 
 const routeMap = {
     '/': '/home/index.html'
 }
 
-function sendFile(res, relativeFilePath){
-    const fullFilePath = path.join(viewsPath, relativeFilePath);
+const mimeTypeMap = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.json': 'application/json'
+}
+
+function sendFile(res, fullFilePath){
+
+    const fileExt = path.extname(fullFilePath)
+    const type = mimeTypeMap[fileExt]
+
     fs.readFile(fullFilePath, function(err, data) {
         if (err) {
             const {message} = err;
@@ -23,14 +34,21 @@ function sendFile(res, relativeFilePath){
         }
         res.writeHead(200, {
             'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'text/html'
+            'Content-Type': type || 'text/plain'
         }).end(data)
     })
 }
 
 function httpHandler(req, res){
-    const path = url.parse(req.url).path;
-    const fileRelativePath = routeMap[path];
+    const pathname = url.parse(req.url).pathname;
+
+    if (pathname.includes(`/${config.staticFilesDir}/`)) {
+        const fullStaticFilePath = path.resolve(pathname.slice(1))
+        sendFile(res, fullStaticFilePath)
+        return;
+    }
+
+    const fileRelativePath = routeMap[pathname];
 
     if(fileRelativePath == undefined){
         const data = 'Not Found'
@@ -40,7 +58,9 @@ function httpHandler(req, res){
         }).end(data)
         return;
     }
-    sendFile(res, fileRelativePath)
+
+    const fullFilePath = path.join(viewsPath, fileRelativePath);
+    sendFile(res, fullFilePath)
 }
 
 http.createServer(httpHandler).listen(config.port, () => {
