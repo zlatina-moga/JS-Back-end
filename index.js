0aaae7198ec2b49eb0157861c858f0039cb932e2
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-06-15 18:24:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const {init: storage} = require('./models/storage')
+
+const {catalog} = require('./controllers/catalog');
+const {about} = require('./controllers/about');
+const {create, post} = require('./controllers/create')
+const {details} = require('./controllers/details')
+const {notFound} = require('./controllers/notFound')
+
+start()
+
+async function start(){
+    const app = express();
+    const port = 3000;
+
+    app.engine('hbs', hbs({
+        extname: '.hbs'
+    }))
+
+    app.set('view engine', 'hbs')
+    app.use('/static', express.static('static'))
+    app.use(await storage())
+
+    app.get('/', catalog);
+    app.get('/about', about);
+    app.get('/details/:id', details);
+    app.get('/create', create)
+    app.post('/create', post)
+
+    app.all('*', notFound)
+
+    app.listen(port, () => console.log(`Server listening on port ${port}`))
+}
