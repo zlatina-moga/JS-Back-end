39c436692a8fd0cf918810cc8319211c904bc741
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-06-30 19:02:51 +0300

    Update index.js

--- Cat-shelter/index.js
+++ Cat-shelter/index.js
@@ -1,68 +1,22 @@
-const config = require('./config.json')
-const http = require('http')
-const url = require('url')
-const path = require('path');
-const fs = require('fs')
+const http = require('http');
+const router = require('./router');
 
-const viewsPath = path.resolve(config.viewsDir);
-const staticFilesPath = path.resolve(config.staticFilesDir);
+const homeController = require('./controllers/home');
+const addBreed = require('./controllers/addBreed');
+const addCat = require('./controllers/addCat');
 
-const routeMap = {
-    '/': '/home/index.html'
+router.get('/', homeController)
+router.get('/add-breed', addBreed)
+router.get('/add-cat', addCat)
+
+const port =  3000;
+const server = http.createServer(requestHandler)
+
+function requestHandler(req, res){
+    const url = new URL(req.url, 'http://localhost')
+    
+    const handler = router.match(req.method, url.pathname)
+    handler(req, res)
 }
 
-const mimeTypeMap = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.json': 'application/json'
-}
-
-function sendFile(res, fullFilePath){
-
-    const fileExt = path.extname(fullFilePath)
-    const type = mimeTypeMap[fileExt]
-
-    fs.readFile(fullFilePath, function(err, data) {
-        if (err) {
-            const {message} = err;
-            res.writeHead(500, {
-                'Content-Length': Buffer.byteLength(message),
-                'Content-Type': 'text/plain'
-            }).end(message)
-            return;
-        }
-        res.writeHead(200, {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': type || 'text/plain'
-        }).end(data)
-    })
-}
-
-function httpHandler(req, res){
-    const pathname = url.parse(req.url).pathname;
-
-    if (pathname.includes(`/${config.staticFilesDir}/`)) {
-        const fullStaticFilePath = path.resolve(pathname.slice(1))
-        sendFile(res, fullStaticFilePath)
-        return;
-    }
-
-    const fileRelativePath = routeMap[pathname];
-
-    if(fileRelativePath == undefined){
-        const data = 'Not Found'
-        res.writeHead(404, {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'text/plain'
-        }).end(data)
-        return;
-    }
-
-    const fullFilePath = path.join(viewsPath, fileRelativePath);
-    sendFile(res, fullFilePath)
-}
-
-http.createServer(httpHandler).listen(config.port, () => {
-    console.log(`Server is listening on ${config.port}`)
-})
+server.listen(port, () => console.log(`Server listening on port ${port}`))
