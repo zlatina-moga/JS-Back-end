453f7ebac45e156d24ab95c11d023264885c0ca7
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-02 20:27:50 +0300

    Update addCat.js

--- Cat-shelter/controllers/addCat.js
+++ Cat-shelter/controllers/addCat.js
@@ -1,7 +1,26 @@
+const parseForm = require('../formParser')
 const loadTemplate = require('../template');
+const database = require('../database/cats')
 
-module.exports = async(req, res) => {
+async function addCat(req, res){
     const addCatPage = await loadTemplate('addCat')
     res.write(addCatPage)
     res.end()
+}
+
+async function createCat(req, res){
+    const body = parseForm(req)
+    console.log('cat added')
+
+    database.addCat(body)
+
+    res.writeHead(301, {
+        'Location': '/'
+    })
+    res.end()
+}
+
+module.exports = {
+    addCat,
+    createCat
 }
