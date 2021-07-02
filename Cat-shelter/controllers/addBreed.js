7ac9e97dd85c045f1f7f2a610e1d314ea2dab24f
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-02 20:27:31 +0300

    Update addBreed.js

--- Cat-shelter/controllers/addBreed.js
+++ Cat-shelter/controllers/addBreed.js
@@ -1,7 +1,26 @@
+const parseForm = require('../formParser')
 const loadTemplate = require('../template');
+const database = require('../database/breeds')
 
-module.exports = async(req, res) => {
+async function addBreed(req, res){
     const addBreedPage = await loadTemplate('addBreed')
     res.write(addBreedPage)
     res.end()
+}
+
+async function createBreed(req, res){
+    const body = await(parseForm(req))
+    console.log('breed added')
+
+    database.push(body)
+        
+    res.writeHead(301, {
+        'Location': '/'
+    })
+    res.end() 
+}
+
+module.exports = {
+    addBreed,
+    createBreed
 }
