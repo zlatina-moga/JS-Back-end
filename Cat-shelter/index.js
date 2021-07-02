489262d9a7e1243581a2f863f4d7585d7c7502d7
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-02 20:31:17 +0300

    Add files via upload

--- Cat-shelter/index.js
+++ Cat-shelter/index.js
@@ -2,12 +2,19 @@ const http = require('http');
 const router = require('./router');
 
 const homeController = require('./controllers/home');
-const addBreed = require('./controllers/addBreed');
-const addCat = require('./controllers/addCat');
+const {addBreed, createBreed} = require('./controllers/addBreed');
+const {addCat, createCat } = require('./controllers/addCat');
+const { deleteCat, shelterCat } = require('./controllers/shelterCat');
 
 router.get('/', homeController)
 router.get('/add-breed', addBreed)
 router.get('/add-cat', addCat)
+router.get('/shelter-cat', shelterCat)
+
+router.post('/add-breed', createBreed);
+router.post('/add-cat', createCat)
+
+router.delete('shelter-cat', deleteCat)
 
 const port =  3000;
 const server = http.createServer(requestHandler)
@@ -15,7 +22,7 @@ const server = http.createServer(requestHandler)
 function requestHandler(req, res){
     const url = new URL(req.url, 'http://localhost')
     
-    const handler = router.match(req.method, url.pathname)
+    const handler = router.match(req.method, url.pathname)   
     handler(req, res)
 }
 
