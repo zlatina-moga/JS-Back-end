169af564219359d96b4496cc0dad565d541fe0d3
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-13 13:25:40 +0300

    Update index.js

--- Cat-shelter/index.js
+++ Cat-shelter/index.js
@@ -4,8 +4,7 @@ const path = require('path')
 
 const homeController = require('./controllers/home');
 const addBreed = require('./controllers/addBreed');
-const {addCat, createCat } = require('./controllers/addCat');
-const { deleteCat, shelterCat } = require('./controllers/shelterCat');
+const addCat = require('./controllers/addCat');
 
 router.get('/', homeController)
 router.get('/add-breed', addBreed)
@@ -13,9 +12,6 @@ router.get('/add-cat', addCat)
 router.get('/shelter-cat', shelterCat)
 
 router.post('/add-breed', addBreed);
-router.post('/add-cat', createCat)
-
-router.delete('shelter-cat', deleteCat)
 
 const PORT =  process.env.PORT || 3000;
 const server = http.createServer(requestHandler)
