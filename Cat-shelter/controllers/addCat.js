03801459a8e828f633e749851e348a44fc2d9bbf
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-13 13:25:00 +0300

    Update addCat.js

--- Cat-shelter/controllers/addCat.js
+++ Cat-shelter/controllers/addCat.js
@@ -1,26 +1,25 @@
-const parseForm = require('../formParser')
+const path = require('path')
+const fs = require('fs/promises')
 const {loadTemplate, renderLayout} = require('../template');
-const database = require('../database/cats')
 
-async function addCat(req, res){
-    const addCatPage = await loadTemplate('addCat')
-    res.write(await renderLayout(addCatPage))
-    res.end()
+module.exports = async(req, res) => {
+    const catsDataFilePath = path.join(__dirname, '../database/cats.json')
+    const breedsDataFilePath = path.join(__dirname, '../database/breeds.json')
+
+    const catsDataBuffer = await fs.readFile(catsDataFilePath);
+    const breedsDataBuffer = await fs.readFile(breedsDataFilePath);
+
+    const catsData = JSON.parse(catsDataBuffer);
+    const breedsData = JSON.parse(breedsDataBuffer);
+
+    if (req.method === 'GET'){       
+        let addCatPage = await loadTemplate('addCat')
+        addCatPage = addCatPage.replace(
+            new RegExp('{{options}}', 'g'), 
+            breedsData.map(breed => `<option value="${breed}">${breed}</option>`))
+
+        res.write(await renderLayout(addCatPage))
+        res.end()
+    } 
 }
 
-async function createCat(req, res){
-    const body = parseForm(req)
-    console.log('cat added')
-
-    database.addCat(body)
-
-    res.writeHead(301, {
-        'Location': '/'
-    })
-    res.end()
-}
-
-module.exports = {
-    addCat,
-    createCat
-}
