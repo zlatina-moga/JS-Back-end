e4c84ad3c3821a24b05565c005577072ef33519d
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-14 18:09:12 +0300

    Update generateID.js

--- Cat-shelter/generateID.js
+++ Cat-shelter/generateID.js
@@ -1,25 +1,18 @@
-const catsDB = {};
+const path = require('path')
+const fs = require('fs/promises')
 
-function nextID(){
+module.exports = async() => {
+    const idList = [];
+
+    const catsDataBuffer = await fs.readFile(path.join(__dirname, '../database/cats.json'));
+    JSON.parse(catsDataBuffer.toString().map(c => idList.push(c.id)))
+    
     let id;
     do {
         id = ('00000000' + (Math.random() * 99999999 | 0).toString(16)).slice(-8);
     }
-    while (catsDB[id] != undefined);
+    while (idList[id] != undefined);
 
     return id;
 }
 
-function addCat(item){
-    catsDB[nextID()] = item
-}
-
-function deleteCat(id){
-    delete catsDB[id]
-}
-
-module.exports = {
-    addCat,
-    deleteCat, 
-    catsDB
-}
