0ab1801532b3f67a27ba26c69afcae4751fea985
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-14 09:05:19 +0300

    Update staticContent.js

--- Cat-shelter/controllers/staticContent.js
+++ Cat-shelter/controllers/staticContent.js
@@ -1,41 +1,18 @@
-const fs = require('fs')
+const fs = require('fs/promises');
 
-module.exports = (req, res) => {
-    const pathname = req.url;
+module.exports = async (req, res) => {
+    const fileLocation = req.url.slice(8);
+    let type = req.url.split('.').pop();
 
-    let file;
+    type = type === 'css' ? 'text/css' : type;
 
-    if (pathname.startsWith('/content/styles')){
-        file = fs.createReadStream(`.${pathname}`)
-    } else if (pathname.startsWith('/content/images')){
-        file = fs.createReadStream(`.${pathname}`)
-    }
-    
-    let type;
-
-    if(pathname.endsWith('css')){
-        type = 'text/css'
-    } else if (pathname.endsWith('jpg') || pathname.endsWith('jpeg')){
-        type = 'image/jpeg';
-    } else if (pathname.endsWith('png')){
-        type = 'image/png'
-    } else if (pathname.endsWith('ico')){
-        type = 'image/x-icon'
-    } else {
-        type = 'text/plain'
-    }
-        
-    file.on('error', () => {
+    try {
+        const file = await fs.readFile(`./content/${fileLocation}`)
+        res.writeHead(200, {'Content-Type': type})
+        res.end(file)
+    } catch (err){
         res.statusCode = 404;
         res.write('Page not found')
         res.end()
-    })
-
-     file.on('data', data => res.write(data))
-     file.on('end', () => {
-         res.writeHead(200, {
-             'Content-Type': type
-         });
-        res.end()
-     })
+    }
 }
