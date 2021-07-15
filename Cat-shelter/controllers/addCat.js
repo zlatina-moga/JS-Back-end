381deac797d27e8d779b0468b49265feea1b911b
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-15 12:40:22 +0300

    Add files via upload

--- Cat-shelter/controllers/addCat.js
+++ Cat-shelter/controllers/addCat.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const fs = require('fs/promises')
+const generateId = require('../generateID')
+const parser = require('../parser')
 const {loadTemplate, renderLayout} = require('../template');
 
-module.exports = async(req, res) => {
+module.exports = async (req, res) => {
     const catsDataFilePath = path.join(__dirname, '../database/cats.json')
     const breedsDataFilePath = path.join(__dirname, '../database/breeds.json')
 
@@ -12,14 +14,35 @@ module.exports = async(req, res) => {
     const catsData = JSON.parse(catsDataBuffer);
     const breedsData = JSON.parse(breedsDataBuffer);
 
-    if (req.method === 'GET'){       
+    if (req.method === 'GET'){
         let addCatPage = await loadTemplate('addCat')
         addCatPage = addCatPage.replace(
             new RegExp('{{options}}', 'g'), 
-            breedsData.map(breed => `<option value="${breed}">${breed}</option>`))
+            breedsData.map(breed => `<option value="${breed}">${breed}</option>`)
+        );
 
         res.write(await renderLayout(addCatPage))
         res.end()
-    } 
+    }
+    else if (req.method === 'POST'){
+        try {
+            const data = await parser(req);
+            data.id = await generateId();
+            catsData.unshift(data)
+
+            await fs.writeFile(catsDataFilePath, JSON.stringify(catsData))
+            console.log(`${data.name} cat added`)
+
+            res.writeHead(302, {
+                'Location': '/'
+            })
+            res.end() 
+        }  catch(err){
+            res.statusCode = 500;
+            res.write('Internal server error')
+            console.log(err)
+            res.end()
+        }
+    }
 }
 
