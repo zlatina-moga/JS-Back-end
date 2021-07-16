30fd302787f4960f6a9cdeb8352b210855a2b6c8
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-16 14:35:14 +0300

    Update home.js

--- Cat-shelter/controllers/home.js
+++ Cat-shelter/controllers/home.js
@@ -1,7 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path')
 const {loadTemplate, renderLayout} = require('../template');
+const createRecord = require('../createCatRecord')
 
 module.exports = async(req, res) => {
-    const homePage = await loadTemplate('index')
-    res.write(await renderLayout(homePage))
-    res.end()
+    try {
+        const catsDataFilePath = path.join(__dirname, '../database/cats.json')
+        const catsDataBuffer = await fs.readFile(catsDataFilePath)
+        const catsData = JSON.parse(catsDataBuffer).map(createRecord).join('');
+        
+        let homePage = await loadTemplate('index')
+        homePage = homePage.replace(new RegExp('{{cats}}', 'g'), catsData)
+
+        res.write(await renderLayout(homePage))
+        res.end()
+    } catch (err){
+        res.statusCode = 500;
+        res.write('Internal server error')
+        console.log(err)
+        res.end()
+    }
+    
 }
