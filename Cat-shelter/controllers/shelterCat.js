f7beea3e73f0c5090992e813a69a6315fdfe9aac
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-07-16 14:35:33 +0300

    Update shelterCat.js

--- Cat-shelter/controllers/shelterCat.js
+++ Cat-shelter/controllers/shelterCat.js
@@ -1,23 +1,34 @@
+const path = require('path')
+const fs = require('fs/promises')
 const {loadTemplate, renderLayout} = require('../template');
-const database = require('../database/cats');
 
-async function shelterCat(req, res){
-    const shelterPage = await loadTemplate('catShelter')
-    res.write(await renderLayout (shelterPage));
-    res.end()
-}
 
-async function deleteCat(req, res){
-    const id = req.url.split('=')[1];
-    database.deleteCat(id)
+module.exports = async (req, res) => {
+    try {
+        const catsDataFilePath = path.join(__dirname, '../database/cats.json')
+        const catsDataBuffer = await fs.readFile(catsDataFilePath)
+        let catsData = JSON.parse(catsDataBuffer)
 
-    res.writeHead(301, {
-        'Location': '/'
-    });
-    res.end()
-}
+        const id = req.url.split('=')[1];
+        const index = catsData.findIndex((e) => e.id === id);
+        const imageFilePath = path.join(__dirname, '../content/images', catsData[index].image)
 
-module.exports = {
-    shelterCat,
-    deleteCat
+        console.log(`${catsData[index].name} just found a new home.`)
+        
+        catsData[index] = null;
+        catsData = catsData.filter(Boolean)
+
+        await fs.rm(imageFilePath)
+        await fs.writeFile(catsDataFilePath, JSON.stringify(catsData))
+    
+        res.writeHead(302, {
+            'Location': '/'
+        });
+        res.end()
+    } catch (err){
+        res.statusCode = 500;
+        res.write('Internal server error')
+        console.log(err)
+        res.end()
+    }
 }
