37618e8fe73fef3a5660ce3f564ab8e66b37bcad
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-08-27 21:21:51 +0300

    Add files via upload

--- Theaters/models/Play.js
+++ Theaters/models/Play.js
@@ -1,13 +1,14 @@
 const {Schema, model} = require('mongoose');
 
 const schema = new Schema({
-    title: {type: String, required: [true, 'Title required']},
-    description: {type: String, required: [true, 'Description required'], maxLength: [50, 'Description must be less than 50 characters long']},
+    title: {type: String, required: true}, //add unique validation
+    description: {type: String, required: true, maxLength: [50, 'Description cannot exceed 50 characters']},
     imageUrl: {type: String, required: true, match:[/^https?/, 'Image must be valid URL']},
     public: {type: Boolean, default: false},
     createdAt: {type: Date, default: Date.now},
-    usersLiked: {type: Schema.Types.ObjectId, ref: 'User'},
-    author: {type: Schema.Types.ObjectId, ref: 'User'}
+    likedBy: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    author: {type: Schema.Types.ObjectId, ref: 'User'},
+    likes: {type: Number}
 })
 
 module.exports = model('Play', schema)
--- Theaters/models/User.js
+++ Theaters/models/User.js
@@ -1,9 +1,9 @@
 const {Schema, model} = require('mongoose');
 
 const schema = new Schema({
-    username: {type: String, required: [true, 'All fields are required']},
+    username: {type: String, required: true},
     hashedPassword: {type: String, required: true},
-    likedPlays: [{type: Schema.Types.ObjectId, ref: 'Play'}]
+    likedPlays: []
 })
 
-module.exports = model('User', schema);
+module.exports = model('User', schema)
