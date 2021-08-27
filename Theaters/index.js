7e67c813489a454a3c2aed9fad8578190eca5608
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-08-27 21:23:59 +0300

    Add files via upload

--- Theaters/index.js
+++ Theaters/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const {PORT} = require('./config');
+const { PORT } = require('./config');
+const databaseConfig = require('./config/database');
 const expressConfig = require('./config/express');
-const routesConfig = require('./config/routes');
-const databaseConfig = require('./config/database')
+const routesConfig = require('./config/routes')
 
 start()
 
 async function start(){
     const app = express();
-    
+
     await databaseConfig(app)
-    expressConfig(app);
+    expressConfig(app)
     routesConfig(app)
 
-    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+    app.listen(PORT, () => {
+        console.log(`Application started at http://localhost:${PORT}`)
+    })
 }
